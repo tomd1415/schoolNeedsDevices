10407c00ccc7e4f9dd4ca6eab80cbc349ec1070c
tomd1415 <33547349+tomd1415@users.noreply.github.com>
2025-02-27 19:43:05 +0000

    Create pupilModel.js

---
+++ server/models/pupilModel.js
@@ -0,0 +1,39 @@
+const pool = require('../config/db');
+
+const getPupils = async () => {
+  const result = await pool.query('SELECT * FROM pupil ORDER BY last_name, first_name');
+  return result.rows;
+};
+
+const getPupilById = async (id) => {
+  const result = await pool.query('SELECT * FROM pupil WHERE pupil_id = $1', [id]);
+  return result.rows[0];
+};
+
+const addPupil = async (first_name, last_name, form_id, notes) => {
+  const result = await pool.query(
+    'INSERT INTO pupil (first_name, last_name, form_id, notes) VALUES ($1, $2, $3, $4) RETURNING *',
+    [first_name, last_name, form_id, notes]
+  );
+  return result.rows[0];
+};
+
+const updatePupil = async (id, first_name, last_name, form_id, notes) => {
+  const result = await pool.query(
+    'UPDATE pupil SET first_name = $1, last_name = $2, form_id = $3, notes = $4 WHERE pupil_id = $5 RETURNING *',
+    [first_name, last_name, form_id, notes, id]
+  );
+  return result.rows[0];
+};
+
+const deletePupil = async (id) => {
+  await pool.query('DELETE FROM pupil WHERE pupil_id = $1', [id]);
+};
+
+module.exports = {
+  getPupils,
+  getPupilById,
+  addPupil,
+  updatePupil,
+  deletePupil
+};
